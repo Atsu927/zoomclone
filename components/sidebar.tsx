c600df1b410ae52172ea00241a2070e89f24758f
Atsu <122103592+Atsu927@users.noreply.github.com>
2024-05-25 23:44:42 +0400

    Navbar-and-MobileNavbar-completed
    Completed navbar and mobile navbar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -7,13 +7,16 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation'
 import React from 'react'
 
+/**
+ * Sidebar component that displays a list of links.
+ */
 const Sidebar = () => {
   const pathname = usePathname();
   return (
     <section className="sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]">
       <div className="flex flex-1 flex-col gap-6">
         {sidebarLinks.map((link) => {
-          const isActive = pathname === link.route;
+          const isActive = pathname === link.route || pathname.startsWith(`${link.route}/`);
 
           return (
             <Link 
