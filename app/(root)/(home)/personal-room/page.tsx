40691bccbe4c9e695601d26817288188d64ceeb8
Atsu <122103592+Atsu927@users.noreply.github.com>
2024-05-25 17:42:01 +0400

    Sidebar-completion
    Completed Sidebar with minor bugs

--- app/(root)/(home)/personal-room/page.tsx
+++ app/(root)/(home)/personal-room/page.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Home = () => {
+const PersonalRoom = () => {
   return (
     <section className="flex size-full flex-col gap-10 text-white">
       <h1 className="text-3xl font-bold">
-        Home
+        PersonalRoom
       </h1>  
 
       
@@ -12,4 +12,4 @@ const Home = () => {
   )
 }
 
-export default Home
+export default PersonalRoom
